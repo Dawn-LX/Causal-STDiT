fccd53c2dbb8ff1f21a2030e5382bda3068e7437
gkf <kite_phone@zju.edu.cn>
2024-08-26 07:19:56 -0400

    update

--- scripts/train.sh
+++ scripts/train.sh
@@ -4,14 +4,24 @@ export PYTHONPATH=$PYTHONPATH:$CODE_ROOT
 cd $CODE_ROOT
 pwd
 
+if test -d "/data"; then
+    # for server 10.130.129.11
+    ROOT_OUTPUT_DIR="/data/CausalSTDiT_working_dir"
+else
+    # for server 10.130.129.34
+    ROOT_OUTPUT_DIR="/data9T/gaokaifeng/CausalSTDiT_working_dir"
+fi
+
+
 
 CFG_PATH=${1}
-# EXP_NAME=${2}
-MASTER_PORT=${2}
-export CUDA_VISIBLE_DEVICES=${3}
+EXP_NAME=${2}
+MASTER_PORT=${3}
+export CUDA_VISIBLE_DEVICES=${4}
 NUM_GPUS=$(echo $CUDA_VISIBLE_DEVICES | awk -F',' '{print NF}')
 
-export IS_DEBUG=1
+
+export IS_DEBUG=0
 export DEBUG_COND_LEN=1
 export DEBUG_WITHOUT_LOAD_PRETRAINED=0
 export TOKENIZERS_PARALLELISM=false
@@ -20,9 +30,16 @@ torchrun \
     --master-port=$MASTER_PORT \
     --nproc-per-node=$NUM_GPUS \
     scripts/train.py \
-    --config $CODE_ROOT/$CFG_PATH
+    --config $CODE_ROOT/$CFG_PATH \
+    --outputs $ROOT_OUTPUT_DIR/$EXP_NAME
 
 <<comment
+
+outputs = f"/data/CausalSTDiT_working_dir/exp4_ParitalCausal{_exp_tag}pp3_tpe33_timelapse_rmVidPadding"
+
+_ROOT_CKPT_DIR = os.getenv("ROOT_CKPT_DIR","/home/gkf/LargeModelWeightsFromHuggingFace") # or /data9T/gaokaifeng/LargeModelWeightsFromHuggingFace
+_ROOT_DATA_DIR = os.getenv("ROOT_DATA_DIR","/data")  #
+# /data/SkyTimelapse or /data9T/gaokaifeng/datasets/SkyTimelapse
 
 # debug overfit
 
@@ -34,23 +51,6 @@ torchrun \
 
     bash scripts/train.sh \
     configs/causal_stdit/train_SkyTimelapse_33x256x256_TPE33.py \
-    9686 0
-
-# train baseline:
-
-    # full-attn fixed tpe
-    bash /home/gkf/project/CausalSTDiT/scripts/train.sh \
-    configs/baselines/full_attn_fixed_tpe.py \
-    9686 0
-
-    # causla attn fixed tpe
-    bash /home/gkf/project/CausalSTDiT/scripts/train.sh \
-    configs/baselines/exp2_causal_attn_fixed_tpe.py \
-    9686 0
-
-    # full attn cyclic tpe
-    bash /home/gkf/project/CausalSTDiT/scripts/train.sh \
-    configs/baselines/exp3_full_attn_cyclic_tpe64.py \
     9686 0
 
 # train partial-causal:
@@ -65,6 +65,22 @@ torchrun \
         configs/baselines/exp4_partialcausal_attn_cyclic_tpe33.py \
         9686 0
     
+# ablations on  skytimelapse:
 
+    # full-attn fixed tpe
+    bash scripts/train.sh \
+    TODO \
+    9686 0
+
+    # causla attn cyclic tpe
+    bash scripts/train.sh \
+    TODO \
+    9686 0
+
+    # partial causal attn cyclic tpe
+    bash scripts/train.sh \
+    configs/ablations_on_SkyTimelapse/exp5_partialcausal_attn_cyclic_tpe33.py \
+    exp5_partial_causal_CfattnPp3_tpe33 \
+    9686 0
 
 comment
